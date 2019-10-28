e96416c2fa468d616d8f2541c5bca92d32bde747
guowei <guowei@pi2star.com>
2019-10-28 16:13:42 +0800

    add @angular/material and related libraries.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,30 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 
 import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+    MatCheckboxModule, MatButtonModule, MatInputModule, MatAutocompleteModule,
+    MatDatepickerModule, MatFormFieldModule, MatRadioModule, MatSelectModule,
+    MatSliderModule, MatSlideToggleModule, MatMenuModule, MatSidenavModule,
+    MatToolbarModule, MatListModule, MatGridListModule, MatCardModule, MatStepperModule,
+    MatTabsModule, MatExpansionModule, MatButtonToggleModule, MatChipsModule,
+    MatIconModule, MatProgressSpinnerModule, MatProgressBarModule, MatDialogModule,
+    MatTooltipModule, MatSnackBarModule, MatTableModule, MatSortModule, MatPaginatorModule
+} from '@angular/material';
 
 @NgModule({
     declarations: [
         AppComponent
     ],
     imports: [
-        BrowserModule, FormsModule
+        BrowserModule, FormsModule, BrowserAnimationsModule,
+        MatCheckboxModule, MatButtonModule, MatInputModule, MatAutocompleteModule,
+        MatDatepickerModule, MatFormFieldModule, MatRadioModule, MatSelectModule,
+        MatSliderModule, MatSlideToggleModule, MatMenuModule, MatSidenavModule,
+        MatToolbarModule, MatListModule, MatGridListModule, MatCardModule, MatStepperModule,
+        MatTabsModule, MatExpansionModule, MatButtonToggleModule, MatChipsModule,
+        MatIconModule, MatProgressSpinnerModule, MatProgressBarModule, MatDialogModule,
+        MatTooltipModule, MatSnackBarModule, MatTableModule, MatSortModule, MatPaginatorModule
     ],
     providers: [],
     bootstrap: [AppComponent]
